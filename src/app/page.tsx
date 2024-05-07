c119d1da5d74699493a44f3cdeef64d4b0111332
caglartufan <caglartufan09@gmail.com>
2024-05-07 23:17:46 +0300

    Set up Tailwind.css and small code clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import SignInPage from './(auth)/sign-in/page'
+import SignInPage from "./(auth)/sign-in/page";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-       <SignInPage />
-      </div>
-    </main>
+    <div className="bg-black">
+      <SignInPage />
+    </div>
   );
-} 
+}

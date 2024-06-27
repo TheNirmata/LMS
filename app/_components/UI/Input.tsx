e51d7cded10b8f510255e971f4b40d5746575459
caglartufan <caglartufan09@gmail.com>
2024-06-27 19:48:06 +0300

    Input component improvement.

--- app/_components/UI/Input.tsx
+++ app/_components/UI/Input.tsx
@@ -1,7 +1,27 @@
 'use client';
-import { useState } from 'react';
+import { ChangeEventHandler, useRef, useState } from 'react';
 import AsteriskIcon from '../../../public/icons/asterisk.svg';
 import CloseIcon from '../../../public/icons/close.svg';
+
+function ResetButton(
+  props: Readonly<{
+    onReset: () => void;
+  }>
+) {
+  const { onReset } = props;
+
+  const clickHandler = () => {
+    onReset();
+  };
+
+  return (
+    <div className="absolute w-12 h-12 top-1 right-0 flex items-center justify-center">
+      <button type="button" onClick={clickHandler}>
+        <CloseIcon />
+      </button>
+    </div>
+  );
+}
 
 export default function Input(
   props: Readonly<{
@@ -14,7 +34,17 @@ export default function Input(
   }>
 ) {
   const { id, name, label, placeholder, required, error } = props;
+  const inputRef = useRef<HTMLInputElement>(null);
   const [value, setValue] = useState('');
+
+  const changeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValue(event.target.value);
+  };
+
+  const resetHandler = () => {
+    setValue('');
+    inputRef.current?.focus();
+  };
 
   return (
     <div className="flex flex-col w-[32.875rem] mb-[1.875rem]">
@@ -33,18 +63,11 @@ export default function Input(
           placeholder={placeholder}
           required={required}
           value={value}
-          onChange={(event) => {
-            setValue(event.target.value);
-          }}
+          onChange={changeHandler}
           className="w-full border-[3px] border-purple outline-purple outline-[3px] py-[13px] ps-[13px] pe-12 rounded-[4px]"
+          ref={inputRef}
         />
-        {value && (
-          <div className="absolute w-12 h-12 top-1 right-0 flex items-center justify-center">
-            <div className="cursor-pointer">
-              <CloseIcon />
-            </div>
-          </div>
-        )}
+        {value && <ResetButton onReset={resetHandler} />}
       </div>
       {error && <small className="text-error text-xs ms-4 mt-1">{error}</small>}
     </div>
